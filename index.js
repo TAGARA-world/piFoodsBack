7451901e0126a1b364db77521e5ab86991daf892
TAGARA-world <83788974+TAGARA-world@users.noreply.github.com>
2022-07-31 16:56:43 -0300

    modifique el puerto por PORT

--- index.js
+++ index.js
@@ -21,11 +21,12 @@ const server = require('./src/app.js');
 const { getDiets } = require('./src/controllers/Diet.controllers.js');
 
 const { conn } = require('./src/db.js');
+const {PORT} = process.env
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at ${PORT}'); // eslint-disable-line no-console
   });
  await getDiets() 
 });
